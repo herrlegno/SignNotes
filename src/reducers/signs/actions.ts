214deb5e3c38acba93716aa43822e7a4f69d54c3
Alejandro Gonzalez Alonso <alejandrolegno@gmail.com>
2020-02-06 14:51:52 +0000

    Fixes and formats files to match prettier

--- src/reducers/signs/actions.ts
+++ src/reducers/signs/actions.ts
@@ -1,15 +1,20 @@
-import { SignPayload, SignActionType, SIGN_IN, SIGN_OUT } from "./types";
+import {
+  SignPayload,
+  SignActionType,
+  SIGN_IN,
+  SIGN_OUT,
+} from './types';
 
 export function signIn(signature: SignPayload): SignActionType {
   return {
     type: SIGN_IN,
-    payload: signature
+    payload: signature,
   };
 }
 
 export function signOut(signature: SignPayload): SignActionType {
   return {
     type: SIGN_OUT,
-    payload: signature
+    payload: signature,
   };
 }
--- src/views/index.ts
+++ src/views/index.ts
@@ -1,2 +1,2 @@
-export { default as Home } from "./Home";
-export { default as Options } from "./Options";
+export { default as Home } from './Home';
+export { default as Options } from './Options';
