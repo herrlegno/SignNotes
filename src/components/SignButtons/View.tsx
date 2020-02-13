e479b4402df16ab0f7effd545b7f44c08c02569b
Alejandro Gonzalez Alonso <alejandrolegno@gmail.com>
2020-02-13 09:43:54 +0000

    Adds some restriction to sign buttons

--- src/components/SignButtons/View.tsx
+++ src/components/SignButtons/View.tsx
@@ -3,11 +3,23 @@ import { signIn, signOut } from '@app/reducers/signs/actions';
 import { Button } from 'react-bootstrap';
 import moment from '@app/config/moment';
 import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { RootState } from '@app/reducers';
 import { SignButtonsProps } from './index.d';
 import styles from './styles.module.css';
 
 const SignButtons: React.FC<SignButtonsProps> = ({ day }) => {
   const dispatch = useDispatch();
+  const start =
+    useSelector(
+      (state: RootState) =>
+        state.signings[day.format('DD-MM-YYYY')]?.in,
+    ) || -1;
+  const end =
+    useSelector(
+      (state: RootState) =>
+        state.signings[day.format('DD-MM-YYYY')]?.out,
+    ) || -1;
 
   const handleOnSignIn = () => {
     const now = moment();
@@ -36,12 +48,25 @@ const SignButtons: React.FC<SignButtonsProps> = ({ day }) => {
 
     dispatch(signOut(signature));
   };
+
+  const isToday = moment().date() === day.date();
+  const signedIn = start >= 0;
+  const signedOut = end >= 0;
+
   return (
     <div className={styles.container}>
-      <Button variant='success' onClick={handleOnSignIn}>
+      <Button
+        variant='success'
+        onClick={handleOnSignIn}
+        disabled={signedIn || !isToday}
+      >
         Llegada
       </Button>
-      <Button variant='danger' onClick={handleOnSignOut}>
+      <Button
+        variant='danger'
+        onClick={handleOnSignOut}
+        disabled={signedOut || !signedIn || !isToday}
+      >
         Salida
       </Button>
     </div>
